0d44d5185a994883b73f6293e5d059603016cfa8
Lukas <lukas.wanner@web.de>
2021-01-13 16:39:16 +0100

    init

--- vue.config.js
+++ vue.config.js
@@ -8,6 +8,17 @@ module.exports = {
     },
     public: 'https://localhost:8080/'
   },
+  pwa: {
+    "short_name": "Scrabble",
+    "name": "Math-Scrabble LETS PLAY!",
+    "description": "MOST EFFECTIVE WAY TO WASTE YOUR TIME",
+    "start_url": "index.html",
+    "background_color": "#3367D6",
+    "display": "standalone",
+    "scope": "/",
+    "theme_color": "#3367D6",
+    workboxPluginMode: 'InjectManifest',
+  },
   "transpileDependencies": [
     "vuetify"
   ]
